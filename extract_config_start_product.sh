e62529a160197fda1e87789a94c5b4684f797646
Ubuntu <ubuntu@lahiru-stratos.(none)>
2014-11-20 10:04:05 +0000

    adding config script

---
+++ extract_config_start_product.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ -d "stratos/products/stratos/modules/distribution/target" ]; then
+	#cp stratos/products/stratos/modules/distribution/target/
+#copy pack a directory
+fi
+
--- setup_stratos.sh
+++ setup_stratos.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
-./install_software.sh
-./build_source.sh
-
+./install_software.sh > install.log
+./checkout_build_source.sh > build.log
+./extract_config_start_product.sh > config_start.log
